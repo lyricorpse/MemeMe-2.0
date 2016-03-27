35dc6fea60b714d55757a50467dfb7a5140127f3
Lyricorpse <lyricorpse@gmail.com>
2016-03-27 15:48:35 -0500

    Add Meme.swift and correct the reference

---
+++ MemeMe 2.0/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeMe 2.0
+//
+//  Created by Feng Zhu on 3/23/16.
+//  Copyright © 2016 Feng Zhu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topText: String!
+    var bottomText: String!
+    var originalImg: UIImage!
+    var memedImg: UIImage!
+}
